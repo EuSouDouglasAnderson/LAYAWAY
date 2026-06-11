ef6978644e990104050cb47ce75b1079ff7984da
EuSouDouglasAnderson <douglasbolsadeaposta@gmail.com>
2026-06-11 11:24:27 -0300

    Update odds.py

--- src/odds.py
+++ src/odds.py
@@ -1,1 +1,14 @@
 
+def fair_odds(h, d, a):
+    ph = 1 / h
+    pd_ = 1 / d
+    pa = 1 / a
+
+    s = ph + pd_ + pa
+
+    return (
+        s - 1,
+        s / ph,
+        s / pd_,
+        s / pa
+    )
